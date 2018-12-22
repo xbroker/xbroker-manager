159cf3c7022d2093438bd2e617c95db046940b33
Lad Steffko <lsteffko@yahoo.com>
2018-12-22 01:21:54 -0800

    Updates

---
+++ precache-manifest.690367179e7e59c21cf9ecaa7408fbb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8022aba0ce53dcffbaba",
+    "url": "/xbroker-manager/static/js/runtime~main.8022aba0.js"
+  },
+  {
+    "revision": "fc85618101f7ebaa46aa",
+    "url": "/xbroker-manager/static/js/main.fc856181.chunk.js"
+  },
+  {
+    "revision": "cfe81b2221b1e4861a5c",
+    "url": "/xbroker-manager/static/js/1.cfe81b22.chunk.js"
+  },
+  {
+    "revision": "fc85618101f7ebaa46aa",
+    "url": "/xbroker-manager/static/css/main.9307854d.chunk.css"
+  },
+  {
+    "revision": "cfe81b2221b1e4861a5c",
+    "url": "/xbroker-manager/static/css/1.eaa926ae.chunk.css"
+  },
+  {
+    "revision": "da4f83e9b4c1a96fdb6bed0cdec0eba0",
+    "url": "/xbroker-manager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d66236271bb105af581edf2f1cb8a89b.js"
+  "/xbroker-manager/precache-manifest.690367179e7e59c21cf9ecaa7408fbb5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/xbroker-manager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
